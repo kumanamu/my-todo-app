fd5bcc40f3e7cd4747ff9bf5deaaf57fe2a20590
kumanamu <serachstock1@gmail.com>
2025-09-24 10:34:23 +0900

    초기작업중

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,31 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias:{
+      '@' : path.resolve(__dirname, 'src')
+
+
+
+    }
+
+
+  },
+  server:{
+
+    port: 3000,
+    proxy : {
+      //프론트에서 api로 호출하면 >https로컬호스트8080api
+      '/api' : {
+target : 'http://localhost:8080',
+changeOrigin : true,
+
+      }
+    }
+  }
+
 })
